ca75008fbf663d5a4125cc4250a14b762127ab80
Bala-Logesh <balassmp@gmail.com>
2020-08-29 10:59:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoronaService } from './corona.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Covid19-Tracker';
+
+  countries:any;
+  country:any;
+  confirmed:Number;
+  recovered:Number;
+  deaths:Number;
+
+  constructor(private corona:CoronaService){}
+
+  isShow = false;
+ 
+  toggleDisplay() {
+    this.isShow = true;
+  }
+
+  toggleDisplayoff() {
+    this.isShow = false;
+    this.country = "";
+  }
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data) => {
+        this.countries=data;
+    });
+  }
+  getCoronaData(){
+    this.corona.getCoronaRealtimeData(this.country).subscribe((data) => {
+    var index = data.length - 1;
+    this.confirmed = data[index].Confirmed;
+    this.recovered = data[index].Recovered;
+    this.deaths = data[index].Deaths;
+  })
+  }
+
+  getCountry(country:any){
+    this.country = country;
+  }
 }
